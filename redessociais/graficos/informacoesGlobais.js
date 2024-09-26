34e99e030ab28ca9a0f0ea0a5fb8ab82477018e4
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-09-26 19:41:22 -0300

    I am the storm that is approaching

--- redessociais/graficos/informacoesGlobais.js
+++ redessociais/graficos/informacoesGlobais.js
@@ -32,18 +32,45 @@ async function quantidadeUsuarios() {
           marker: {
             color: getComputedStyle(document.body).getPropertyValue('--primary-color')
           }
-  ]
+]
   
-  const grafico = document.createElement('div')
-  grafico.className = 'grafico'
-  document.getElementById('graficos-container').appendChild(grafico)
-  Plotly.newPlot(grafico, data)
+const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
   
+}
+  
+quantidadeUsuarios()
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    title: {
+      text: 'Redes sociais com mais usuários no mundo',
+       x: 0,
+       font: {
+           color: getCSS('--primary-color'),
+           family: getCSS('--font'),
+           size: 30
+       }
+    },
+      xaxis: {
+      
+      },
+      yaxis: {
+      
+      }
+}
+
+const getCSS = (variavel) => {
+    return getComputedStyle(document.body).getPropertyValue(variavel)
   }
   
-  quantidadeUsuarios()
-
-  const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
-}
+  const tickConfig = {
+    color: getCSS('--primary-color'),
+    size: 16,
+    family: getCSS('--font')
+  }
+  
+export { getCSS, tickConfig }
