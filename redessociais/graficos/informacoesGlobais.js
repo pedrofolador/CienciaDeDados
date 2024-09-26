8d16a50bc944587a696db525332d47b39e5ae917
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-09-26 19:45:43 -0300

    BECAUSE I'M BAD

--- redessociais/graficos/informacoesGlobais.js
+++ redessociais/graficos/informacoesGlobais.js
@@ -8,69 +8,13 @@ async function vizualizarInformacoesGlobais() {
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
     const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixed(2)
+
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
 }
 
 vizualizarInformacoesGlobais()
-
-async function quantidadeUsuarios() {
-    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
-    const res = await fetch(url)
-    const dados = await res.json()
-    const nomeDasRedes = Object.keys(dados)
-    const quantidadeUsuarios = Object.values(dados)
-  
-    const data = [
-        {
-          x: 'nomeDasRedes',
-          y: quantidadeDeUsuarios,
-          type: 'bar',
-          marker: {
-            color: getComputedStyle(document.body).getPropertyValue('--primary-color')
-          }
-]
-  
-const grafico = document.createElement('div')
-grafico.className = 'grafico'
-document.getElementById('graficos-container').appendChild(grafico)
-Plotly.newPlot(grafico, data)
-  
-}
-  
-quantidadeUsuarios()
-
-const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color'),
-    title: {
-      text: 'Redes sociais com mais usuários no mundo',
-       x: 0,
-       font: {
-           color: getCSS('--primary-color'),
-           family: getCSS('--font'),
-           size: 30
-       }
-    },
-      xaxis: {
-      
-      },
-      yaxis: {
-      
-      }
-}
-
-const getCSS = (variavel) => {
-    return getComputedStyle(document.body).getPropertyValue(variavel)
-  }
-  
-  const tickConfig = {
-    color: getCSS('--primary-color'),
-    size: 16,
-    family: getCSS('--font')
-  }
-  
-export { getCSS, tickConfig }
