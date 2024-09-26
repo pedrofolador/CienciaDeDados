3010dd34e048e029a94a2361332f9587fc3e1443
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-09-26 19:26:56 -0300

    Longer thingie I suppose

--- redessociais/graficos/informacoesGlobais.js
+++ redessociais/graficos/informacoesGlobais.js
@@ -7,6 +7,9 @@ async function vizualizarInformacoesGlobais() {
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    
+    const container = document.getElementById(‘graficos-container’)
+    container.appendChild(paragrafo)
   }
   
   vizualizarInformacoesGlobais()
