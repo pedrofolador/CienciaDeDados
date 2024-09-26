7c979bf78d957e68970f6c76b1a297f14a202779
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-09-26 19:26:35 -0300

    Small little thingie ig

--- redessociais/graficos/informacoesGlobais.js
+++ redessociais/graficos/informacoesGlobais.js
@@ -1,13 +1,12 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-async function visualizarInformacoesGlobais() {
-
-        const res = await fetch(url)
-
-        const dados = await res.json()
-
-        console.log(dados);
-
-    }
-
-    visualizarInformacoesGlobais()
+async function vizualizarInformacoesGlobais() {
+    const res = await fetch(url)
+    const dados = await res.json()
+    cosole.log(dados);
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container__texto')
+    paragrafo.innerHTML = `Você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+  }
+  
+  vizualizarInformacoesGlobais()
